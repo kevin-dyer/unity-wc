a4b272bc740f521071ff76a4559bd8e95406058e
Andrew Heath <aheath@altair.com>
2019-10-01 16:08:30 -0700

    add demo data to pass into table element

--- src/components/my-table.js
+++ src/components/my-table.js
@@ -7,13 +7,30 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
-
 import { html } from 'lit-element';
 import { PageViewElement } from './page-view-element.js';
 import './table/table.js'
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
+
+// example table data, should eventually turn into controls
+// normally this would be taken from the store
+const exampleData = [
+  {name: 'red', hex: '#ff0000', favorite: true},
+  {name: 'black', hex: '#000000', favorite: true},
+  {name: 'yellow', hex: '#ffff00', favorite: false},
+  {name: 'green', hex: '#00ff00', favorite: true},
+  {name: 'grey', hex: '#888888', favorite: false},
+  {name: 'magenta', hex: '#ff00ff', favorite: false}
+]
+
+const exampleColumns = [
+  {name: 'hex', width: 120},
+  {name: 'name', width: 120},
+  {name: 'favorite', width: 120}
+]
+
 
 class MyTable extends PageViewElement {
   static get styles() {
@@ -23,7 +40,7 @@ class MyTable extends PageViewElement {
   }
 
   render() {
-    return html`<unity-table/>`
+    return html`<unity-table .data="${exampleData}" .columns="${exampleColumns}"/>`
   }
 }
 
