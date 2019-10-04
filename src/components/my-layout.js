f68e94c3247b4bdc8e074436fe9deeb667e24ee2
Kevin Dyer <kevind@candicontrols.com>
2019-10-04 16:46:06 -0700

    CANDI-13657 updated handleBack function to use window.history.back() method. Temporary.

--- src/components/my-layout.js
+++ src/components/my-layout.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'lit-element';
 import { PageViewElement } from './page-view-element.js';
 import './unity-layout/unity-page-header.js'
+import './unity-button/unity-button.js'
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
@@ -13,7 +14,40 @@ class MyLayout extends LitElement {
   }
 
   render() {
-    return html`<unity-page-header title="MOCC2 Title"/>`
+    return html`<unity-page-header
+      title="MOCC2 Title"
+      ?showBackBtn=${true}
+      .tabs=${[
+        {
+          label: 'Users',
+          onClick: (e) => {
+            console.log("Users tab clicked")
+          }
+        },
+        {
+          label: 'Rules',
+          onClick: (e) => {
+            console.log("Rules tab clicked")
+          }
+        },
+        {
+          label: 'API Keys',
+          onClick: (e) => {
+            console.log("API Keys tab clicked")
+          }
+        }
+      ]}
+      .selectedTab=${1}
+    >
+      <div slot="action-content">
+        <unity-button
+          label="my button"
+          ?gradient=${true}
+          ?disabled=${false}
+          @click=${e => console.log("unity-button clicked! e: ", e)}
+        />
+      </div>
+    </unity-page-header>`
   }
 }
 
