c10d3c9df5242afe1014d929f70126e3887f1816
Andrew Heath <aheath@altair.com>
2019-11-05 19:55:11 -0800

    update global-nav example

--- src/components/my-global-nav.js
+++ src/components/my-global-nav.js
@@ -17,9 +17,92 @@ import { SharedStyles } from './shared-styles.js';
 
 // example table data, should eventually turn into controls
 // normally this would be taken from the store
-const exampleData = []
+const topItems = [
+  {
+    key: 'item-0',
+    label: 'Top Item 0',
+    slot: 'top',
+    short: false,
+    icon: 'account-balance'
+  },
+  {
+    key: 'item-1',
+    label: 'Top Item 1',
+    slot: 'top',
+    short: true,
+    icon: 'account-circle'
+  },
+  {
+    key: 'item-2',
+    label: 'Top Item 2',
+    slot: 'top',
+    short: false,
+    icon: 'add-circle-outline'
+  },
+  {
+    key: 'item-3',
+    label: 'Top Item 3',
+    slot: 'top',
+    short: true,
+    icon: 'android'
+  },
+  {
+    key: 'item-4',
+    label: 'Top Item 4',
+    slot: 'top',
+    short: false,
+    icon: 'bug-report'
+  }
+]
+const bottomItems = [
+  {
+    key: 'item-5',
+    label: 'Bottom Item 0',
+    slot: 'bottom',
+    short: true
+  },
+  {
+    key: 'item-6',
+    label: 'Bottom Item 1',
+    slot: 'bottom',
+    short: false
+  },
+  {
+    key: 'item-7',
+    label: 'Bottom Item 2',
+    slot: 'bottom',
+    short: true
+  },
+  {
+    key: 'item-8',
+    label: 'Bottom Item 3',
+    slot: 'bottom',
+    short: true
+  },
+  {
+    key: 'item-9',
+    label: 'Bottom Item 4',
+    slot: 'bottom',
+    short: false
+  }
+]
+
+function reportSelection(key, label) { console.log(`I clicked on key[${key}] with label[${label}]`)}
 
 class MyGlobalNav extends PageViewElement {
+  constructor() {
+    super()
+    this._selected = topItems[0].key
+
+    this._changeSelection = this._changeSelection.bind(this)
+  }
+
+  static get properties() {
+    return {
+      _selected: { type: String }
+    }
+  }
+
   static get styles() {
     return [
       SharedStyles,
@@ -43,6 +126,8 @@ class MyGlobalNav extends PageViewElement {
     ];
   }
 
+  _changeSelection(key) { this._selected = key }
+
   render() {
     return html`
       <div class="example-container">
@@ -50,16 +135,28 @@ class MyGlobalNav extends PageViewElement {
           <div slot="logo">
             This is a logo.
           </div>
-          <unity-global-nav-top-item slot="top" label="Top Item 0"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="top" short label="Top Item 1"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="top" label="Top Item 2"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="top" short label="Top Item 3"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="top" label="Top Item 4"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="bottom" short label="Bottom Item 0"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="bottom" label="Bottom Item 1"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="bottom" short label="Bottom Item 2"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="bottom" short label="Bottom Item 3"></unity-global-nav-top-item>
-          <unity-global-nav-top-item slot="bottom" label="Bottom Item 4"></unity-global-nav-top-item>
+          ${topItems.map(({slot, key, label, short, icon}) => html`
+            <unity-global-nav-top-item
+              slot="${slot}"
+              key="${key}"
+              .onSelect="${this._changeSelection}"
+              label="${label}"
+              icon="${icon}"
+              .short="${short}"
+              .selected="${this._selected === key}"
+            ></unity-global-nav-top-item>
+          `)}
+          ${bottomItems.map(({slot, key, label, short, icon}) => html`
+            <unity-global-nav-top-item
+              slot="${slot}"
+              key="${key}"
+              .onSelect="${this._changeSelection}"
+              label="${label}"
+              icon="${icon}"
+              .short="${short}"
+              .selected="${this._selected === key}"
+            ></unity-global-nav-top-item>
+          `)}
         </unity-global-nav>
       </div>
     `
