2e7cbc26aef1ca40b064c1f9fbee67c3efd917dc
Andrew Heath <aheath@altair.com>
2020-01-27 18:14:12 -0800

    add nowrap bool, make items stretch as intended, needs refactor for borders

--- src/components/unity-section/unity-section.js
+++ src/components/unity-section/unity-section.js
@@ -6,7 +6,7 @@ import { UnityDefaultThemeStyles } from '@bit/smartworks.unity.unity-default-the
 /**
  * A container that will horizontally or vertically with siblings of the same component
  * @name UnitySection
- * @param
+ * @param {bool} nowrap, on row, control if column sections should wrap
  * @returns {LitElement} returns a class extended from LitElement
  * @example
  *   <unity-section
@@ -21,19 +21,23 @@ class UnitySection extends LitElement {
   constructor() {
     super()
 
+    this.nowrap = false
   }
 
   static get properties() {
     return {
+      nowrap: { type: Boolean }
     }
   }
 
 
   getClasses() {
+    const { nowrap } = this
     let classes = ['section']
-    // parent is not a section
-    if (getParent(this).localName !== unity_section) classes.push('top-border')
-    else {
+    if (!!nowrap) classes.push('no-wrap')
+    // parent is a section
+    if (getParent(this).localName === unity_section) {
+      classes.push('top-border')
       // nextSibling is a section
       const next = getNextSibling(this)
       if (!!next && next.localName === unity_section) classes.push('inner-border')
@@ -44,9 +48,9 @@ class UnitySection extends LitElement {
   render() {
     const classes = this.getClasses()
     return html`
-      <div class="${classes}">
+      <!-- <div class="classes"> -->
         <slot></slot>
-      </div>
+      <!-- </div> -->
     `
   }
 
@@ -55,22 +59,27 @@ class UnitySection extends LitElement {
       UnityDefaultThemeStyles,
       css`
         :host {
-          --section-color: var(--background-color, var(--default-background-color, white));
-          --border-color: var(--section-border-color, var(--default-medium-grey-background-color));
+          --section-color: var(--background-color, var(--default-background-color));
+          --border-color: black;
           flex: 1;
-        }
-        .section {
           background-color: var(--section-color);
           display: flex;
           flex-direction: row;
           align-items: center;
+          align-self: stretch;
           justify-content: center;
+          flex-wrap: wrap;
+        }
+        .section {
+        }
+        .no-wrap {
+          flex-wrap: nowrap;
         }
         .top-border {
-          border-top: 1px solid var(--border-color);
+          border-top: 1px solid black;
         }
         .inner-border {
-          border-right: 1px solid var(--border-color);
+          border-right: 1px solid black;
         }
       `
     ]
