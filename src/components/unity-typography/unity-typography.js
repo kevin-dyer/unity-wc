d68457aa158686e481062db5152d125121f1f80b
Andrew Heath <aheath@altair.com>
2020-01-30 15:46:13 -0800

    rename and organize css vars to allow passed in css vars to always override attribute applied classes

--- src/components/unity-typography/unity-typography.js
+++ src/components/unity-typography/unity-typography.js
@@ -114,62 +114,71 @@ class UnityTypography extends LitElement {
       UnityDefaultThemeStyles,
       css`
         :host {
+          --font-face: var(--font-family, var(--default-font-family));
           --font-color-light: var(--light-grey-text-color, var(--default-light-grey-text-color));
           --font-color-medium: var(--medium-grey-text-color, var(--default-medium-grey-text-color));
           --font-color-dark: var(--dark-grey-text-color, var(--default-dark-grey-text-color));
           --font-color-black: var(--black-text-color, var(--default-black-text-color));
-          --default-header1-size: var(--header1-font-size, var(--default-header1-font-size));
-          --default-header2-size: var(--header2-font-size, var(--default-header2-font-size));
-          --default-paragraph-size: var(--paragraph-font-size, var(--default-paragraph-font-size));
-          --default-medium-size: var(--medium-text-size, var(--default-medium-text-size));
-          --default-small-size: var(--small-text-size, var(--default-));
-          --default-header1-weight: var(--header1-font-weight, var(--default-header1-font-weight));
-          --default-header2-weight: var(--header2-font-weight, var(--default-header2-font-weight));
-          --default-paragraph-weight: var(--paragraph-font-weight, var(--default-paragraph-font-weight));
-          --default-medium-weight: var(--medium-text-weight, var(--default-medium-text-weight));
-          --default-small-weight: var(--small-text-weight, var(--default-small-text-weight));
+          --header1-size: var(--header1-font-size, var(--default-header1-font-size));
+          --header2-size: var(--header2-font-size, var(--default-header2-font-size));
+          --paragraph-size: var(--paragraph-font-size, var(--default-paragraph-font-size));
+          --medium-size: var(--medium-text-size, var(--default-medium-text-size));
+          --small-size: var(--small-text-size, var(--default-));
+          --header1-weight: var(--header1-font-weight, var(--default-header1-font-weight));
+          --header2-weight: var(--header2-font-weight, var(--default-header2-font-weight));
+          --paragraph-weight: var(--paragraph-font-weight, var(--default-paragraph-font-weight));
+          --medium-weight: var(--medium-text-weight, var(--default-medium-text-weight));
+          --small-weight: var(--small-text-weight, var(--default-small-text-weight));
+
+          --font-size: var(--font-size, var(--paragraph-size));
         }
-        color-light {
+        div {
+          font-family: var(--font-face, var(--default-font-family));
+          font-size: var(--font-size, var(--paragraph-weight));
+          font-weight: var(--font-weight, var(--paragraph-weight));
+          color: var(--font-color, var(--font-color-black));
+        }
+        .color-light {
           color: var(--font-color, var(--font-color-light));
         }
-        color-medium {
+        .color-medium {
           color: var(--font-color, var(--font-color-medium));
         }
-        color-dark {
+        .color-dark {
           color: var(--font-color, var(--font-color-dark));
         }
-        color-dark {
-          color: var(--font-black, var(--font-color-black));
+        .color-dark {
+          color: var(--font-color, var(--font-color-black));
         }
-        size-header-1 {
-          font-size: var(--font-size, var(--default-header1-size));
+        .size-header-1 {
+          font-size: var(--font-size, var(--header1-size));
         }
-        size-header-2 {
-          font-size: var(--font-size, var(--default-header2-size));
+        .size-header-2 {
+          font-size: var(--font-size, var(--header2-size));
         }
-        size-paragraph {
-          font-size: var(--font-size, var(--default-paragraph-size));
+        .size-paragraph {
+          font-size: var(--font-size, var(--paragraph-size));
         }
-        size-medium {
-          font-size: var(--font-size, var(--default-medium-size));
+        .size-medium {
+          font-size: var(--font-size, var(--medium-size));
         }
-        size-small {
-          font-size: var(--font-size, var(--default-small-size));
+        .size-small {
+          font-size: var(--font-size, var(--small-size));
         }
-        weight-header-1 {
-          font-weight: var(--font-weight, var(--default-header1-weight));
+        .weight-header-1 {
+          font-weight: var(--font-weight, var(--header1-weight));
         }
-        weight-header-2 {
-          font-weight: var(--font-weight, var(--default-header2-weight));
+        .weight-header-2 {
+          font-weight: var(--font-weight, var(--header2-weight));
         }
-        weight-paragraph {
-          font-weight: var(--font-weight, var(--default-paragraph-weight));
+        .weight-paragraph {
+          font-weight: var(--font-weight, var(--paragraph-weight));
         }
-        weight-medium {
-          font-weight: var(--font-weight, var(--default-medium-weight));
+        .weight-medium {
+          font-weight: var(--font-weight, var(--medium-weight));
         }
-        weight-small {
-          font-weight: var(--font-weight, var(--default-small-weight));
+        .weight-small {
+          font-weight: var(--font-weight, var(--small-weight));
         }
       `
     ]
