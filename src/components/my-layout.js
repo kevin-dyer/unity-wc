393fdf7c761141512e060a127a378325b1b11bd8
Andrew Heath <aheath@altair.com>
2020-01-30 17:29:25 -0800

    add bordered demo to my-layout

--- src/components/my-layout.js
+++ src/components/my-layout.js
@@ -176,14 +176,14 @@ class MyLayout extends LitElement {
         </unity-section>
       </unity-section>
       <unity-section>
-        <unity-section>
-          <div style="height: 400px;">
+        <unity-section bordered>
+          <div style="height: 400px; width: 150px">
             <unity-typography>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </unity-typography>
           </div>
         </unity-section>
-        <unity-section style="--background-color: #eef4FF;">
+        <unity-section bordered style="--background-color: #eef4FF;">
           <div style="height: 500px; width: 500px;">
             <unity-table
               selectable
@@ -200,7 +200,7 @@ class MyLayout extends LitElement {
             </unity-table>
           </div>
         </unity-section>
-        <unity-section>
+        <unity-section bordered>
           <div style="height: 400px; overflow-y: scroll">
             <unity-typography style="--font-size: 42px; --font-family: Avenir; --font-color: red; --font-weight: 800;" size="header1" color="light">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
