7190d4fb17fcf3a10f34d745b03a67ea5fbcbe98
Alberto <avicente@europe.altair.com>
2020-03-16 13:52:46 +0100

    unity-tooltip component

---
+++ test/unit/unity-tooltip.test.js
@@ -0,0 +1,17 @@
+import { fixture, expect } from '@open-wc/testing';
+import '../../src/components/unity-tooltip/unity-tooltip';
+
+describe('tooltip test', () => {
+  it('without arrow', async () => {
+    const el = await fixture('<unity-tooltip label="hi"></unity-tooltip>');
+    expect(el).shadowDom.to.equal('<span class="tooltip"><unity-typography size="paragraph">hi<unity-typography></span>');
+  });
+});
+
+describe('tooltip test', () => {
+  it('with arrow', async () => {
+    const el = await fixture('<unity-tooltip arrow="bottom" label="hi"></unity-tooltip>');
+    const span = el.shadowRoot.querySelector('span')
+    expect(span.className).to.include('arrow bottom');
+  });
+});
