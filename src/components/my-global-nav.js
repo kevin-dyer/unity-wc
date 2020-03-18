a19d5a155a7f743cfe99456490b0bae65b03a66b
Alberto <avicente@europe.altair.com>
2020-03-18 12:48:45 +0100

    updated app to use global nav bit component

--- src/components/my-global-nav.js
+++ src/components/my-global-nav.js
@@ -9,7 +9,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 */
 import { html, css } from 'lit-element';
 import { PageViewElement } from './page-view-element.js';
-import './unity-global-nav/unity-global-nav-base.js'
+import '@bit/smartworks.unity.unity-global-nav-base'
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
