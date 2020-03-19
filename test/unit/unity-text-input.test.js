3e7913f11001b566f303c1f6209b4f15f9feadfb
Andrew Heath <aheath@altair.com>
2020-03-19 16:36:27 -0700

    add maxlength, charcount/maxlength, and disabled tests

--- test/unit/unity-text-input.test.js
+++ test/unit/unity-text-input.test.js
@@ -53,6 +53,39 @@ describe('unity-text-input', () => {
       expect(Number(charCount.innerText)).to.equal(testText.length)
     })
 
+    it('should have a character limit', async () => {
+      const testInput = "i am greater than twenty characters"
+      const maxlength = 20
+      const el = await fixture(`<unity-text-input value="${testInput}" maxlength="${maxlength}"></unity-text-input>`)
+      const ironInput = el.shadowRoot.querySelector('iron-input.input-wrapper')
+      const input = el.shadowRoot.querySelector('input#input')
+      expect(input.maxLength).to.equal(maxlength)
+      expect(ironInput.bindValue.length).to.equal(maxlength)
+      expect(ironInput.bindValue).to.not.equal(testInput)
+      expect(ironInput.bindValue).to.equal(testInput.slice(0, maxlength))
+    })
+
+    it('should show current and max character count', async () => {
+      const maxlength = 50
+      const lessInput = "i am less than fifty characters"
+      const moreInput = "i am most definitely in excess of a number of characters that equals fifty"
+      const less = await fixture(`<unity-text-input value="${lessInput}" charCount maxlength="${maxlength}"></unity-text-input>`)
+      let charCount = less.shadowRoot.querySelector('div.bottom span.charCount')
+      expect(charCount.innerText).to.include(`${lessInput.length}/${maxlength}`)
+      const more = await fixture(`<unity-text-input value="${moreInput}" charCount maxlength="${maxlength}"></unity-text-input>`)
+      charCount = more.shadowRoot.querySelector('div.bottom span.charCount')
+      expect(charCount.innerText).to.not.include(`${moreInput.length}/${maxlength}`)
+      expect(charCount.innerText).to.include(`${moreInput.slice(0, maxlength).length}/${maxlength}`)
+    })
+
+    it('should have disabled', async () => {
+      const el = await fixture('<unity-text-input disabled></unity-text-input>')
+      const ironInput = el.shadowRoot.querySelector('iron-input.input-wrapper.disabled')
+      const input = el.shadowRoot.querySelector('input#input.disabled')
+      expect(ironInput).to.exist
+      expect(input).to.exist
+      expect(input.disabled).to.be.true
+    })
   })
 })
 
