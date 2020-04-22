8407d321fccad8063ab52b56d553030b5cb1490e
Henry Raschke <hraschke@altair.com>
2020-04-22 12:39:08 -0700

    Got rid of old naming convention

--- src/components/unity-table/unity-table-export.js
+++ src/components/unity-table/unity-table-export.js
@@ -8,21 +8,12 @@ import { UnityDefaultThemeStyles } from '@bit/smartworks.unity.unity-default-the
 /**
  * Displays button which will download a csv file when pressed.
  * @name UnityTableExport
- * @param {[]} headers, headers for the columns in your data, should be an array of strings that match the keys in your table/data. If not provided, these will be generated automatically
- * @param {[]} data, data to be exported as csv; should be either an array of arrays that contain strings (won't be sorted) or an array of objects keyed with strings that match 'headers'
- * @param {ref} tableRef, a reference object to a unity-table element that, if provided, will define the data to be  exported (alternative to 'data' attribute)
+ * @param {ref} tableRef, a reference object to a unity-table element that will define the data to be exported
  * @param {func} onExport, callback that fired with result of export. Returns an object with keys "success," "exportedData," and "clickEvent"
- * @param {''} buttonType, action button type for styling ('solid', 'gradient', 'outlined'), default ''
  * @returns {LitElement} returns a class extended from LitElement
  * @example
  *  <unity-table-export
- *    buttonType="solid"
- *    .headers="${['Key', 'Label', 'Note']}"
- *    .data="${[
- *      ['row1key','Row 1 Label', 'This is row 1 data'],
- *      ['row2key','Row 2 Label', 'This is row 2 data'],
- *      ['row3key','Row 3 Label', 'This is row 3 data'],
- *    ]}"
+ *    .tableRef="${tableRef}"
  *    .onExport="${({ success, exportedData, clickEvent }) => !!success && console.log('CSV Exported Successfully!')}"
  *  />
  */
@@ -30,15 +21,13 @@ import { UnityDefaultThemeStyles } from '@bit/smartworks.unity.unity-default-the
 class UnityTableExport extends LitElement {
   constructor() {
     super()
-    this.headers = []
-    this.data = []
-    this.buttonType = ''
     this.onExport = ()=>{}
     this.tableRef = null
 
-    this._autoAddColumns = false
+    this._headers = []
 
     this.handleClick = this.handleClick.bind(this)
+    this.downloadFile = this.downloadFile.bind(this)
     this.buildDataToExport = this.buildDataToExport.bind(this)
     this.makeRow = this.makeRow.bind(this)
     this.addHeaders = this.addHeaders.bind(this)
@@ -47,34 +36,9 @@ class UnityTableExport extends LitElement {
   // inputs
   static get properties() {
     return {
-      headers: { type: Array },
-      data: { type: Array },
       tableRef: { type: Object },
       onExport: { type: Function },
-      buttonType: { type: String },
     }
-  }
-
-  set headers(headers) {
-    const oldHeaders = this._headers
-    this._headers = headers
-
-    this.requestUpdate('headers', headers)
-  }
-
-  get headers() {
-    return this._headers
-  }
-
-  set data(data) {
-    const oldData = this._data
-    this._data = data
-
-    this.requestUpdate('data', oldData)
-  }
-
-  get data() {
-    return this._data
   }
 
   set tableRef(ref) {
@@ -91,6 +55,15 @@ class UnityTableExport extends LitElement {
   handleClick(e) {
     const data = this.buildDataToExport()
     const csvData = data.map(row => row.map(cell => `\"${cell.toString()}\"`).join(", ")).join("\n")
+
+    this.onExport({
+      success: this.downloadFile(csvData),
+      exportedData: data,
+      clickEvent: e
+    })
+  }
+
+  downloadFile(fileData) {
     // Create an invisible <a> element and click it to trigger download
     // QUESTION: Better way to do this?
     const hiddenLink = document.createElement('a')
@@ -100,18 +73,16 @@ class UnityTableExport extends LitElement {
     document.body.appendChild(hiddenLink)
     hiddenLink.click()
     document.body.removeChild(hiddenLink)
-    // TODO: Detect and handle failed exports
-    this.onExport({
-      success: true,
-      exportedData: data,
-      clickEvent: e
-    })
+    // TODO: Detect and handle failed exports and return
+    return true
   }
 
   buildDataToExport() {
-    if (!this.headers || this.headers.length === 0) this._autoAddColumns = true
-    const rowsData = this.data.map(this.makeRow)
-    const headers = this.headers
+    const tableData = this.tableRef._flattenedData || []
+    console.log('this.tableRef', this.tableRef)
+    console.log('tableData', tableData)
+    const rowsData = tableData.map(this.makeRow)
+    const headers = this._headers
     return [ headers, ...rowsData]
   }
 
@@ -122,14 +93,13 @@ class UnityTableExport extends LitElement {
       console.error(`invalid row passed to makeRowArrayFromObject: `, row)
       return []
     }
-    if (!!this._autoAddColumns) this.addHeaders(row)
+    this.addHeaders(row)
     return this._headers.map(header => row.hasOwnProperty(header) ? row[header].toString() : '')
   }
 
   addHeaders(row) {
-    // QUESTION: Is it okay to retrieve and set the internal "_headers" here?
     const oldHeaders = this._headers
-    const newHeaders = Object.keys(row).filter(header => !this.headers.includes(header))
+    const newHeaders = Object.keys(row).filter(header => !this._headers.includes(header))
     const allHeaders = [ ...oldHeaders, ...newHeaders ]
     this._headers = [ ...oldHeaders, ...newHeaders ]
   }
@@ -137,12 +107,11 @@ class UnityTableExport extends LitElement {
   render() {
     // TODO: Ingest button styling / icons as attributes || take any child element(s) and wrap them in a slot?
     return html`
-      <unity-button
-        label="Export"
-        type=${this.buttonType}
+      <div
         @click=${this.handleClick}
       >
-      </unity-button>
+        <slot></slot>
+      </div>
     `
   }
 
