f2b20af5e0a89a869e9dc7c9b1b535206c4518a2
Andrew Heath <aheath@altair.com>
2020-05-05 13:49:24 -0700

    move global-nav demo items to separate file

--- src/components/my-global-nav.js
+++ src/components/my-global-nav.js
@@ -10,105 +10,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 import { html, css } from 'lit-element';
 import { PageViewElement } from './page-view-element.js';
 import '@bit/smartworks.unity.unity-core/unity-global-nav-base'
-
+import { items }'./unity-global-nav/fakeItems.js'
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
-
-// example table data, should eventually turn into controls
-// normally this would be taken from the store
-const items = {
-  top: [
-    {
-      key: 'item-0',
-      label: 'Top Item 0',
-      short: false,
-      icon: 'account-balance',
-      children: [
-        {
-          key: 'item-0-0',
-          label: 'Item 0-0',
-          icon: 'query-builder'
-        },
-        {
-          key: 'item-0-1',
-          label: 'Item 0-1',
-          icon: 'font-download'
-        },
-        {
-          key: 'item-0-2',
-          label: 'Item 0-2',
-          icon: 'gavel'
-        },
-        {
-          key: 'item-0-3',
-          label: 'Item 0-3',
-          icon: 'motorcycle'
-        }
-      ]
-    },
-    {
-      key: 'item-1',
-      label: 'Top Item 1',
-      short: true,
-      icon: 'account-circle'
-    },
-    {
-      key: 'item-2',
-      label: 'Top Item 2',
-      short: false,
-      icon: 'add-circle-outline'
-    },
-    {
-      key: 'item-3',
-      label: 'Top Item 3',
-      short: true,
-      icon: 'android',
-      children: [
-        {
-          key: 'item-3-0',
-          label: 'Item 3-0',
-          icon: 'euro-symbol'
-        }
-      ]
-    },
-    {
-      key: 'item-4',
-      label: 'Top Item 4',
-      short: false,
-      icon: 'bug-report'
-    }
-  ],
-  bottom: [
-    {
-      key: 'item-5',
-      label: 'Bottom Item 0',
-      short: true
-    },
-    {
-      key: 'item-6',
-      label: 'Bottom Item 1',
-      short: false
-    },
-    {
-      key: 'item-7',
-      label: 'Bottom Item 2',
-      short: true
-    },
-    {
-      key: 'item-8',
-      label: 'Bottom Item 3',
-      short: true
-    },
-    {
-      key: 'item-9',
-      label: 'Bottom Item 4',
-      short: false
-    }
-  ]
-}
-
-// function reportSelection(key, label) { console.log(`I clicked on key[${key}] with label[${label}]`)}
 
 class MyGlobalNav extends PageViewElement {
 
---
+++ src/components/unity-global-nav/fakeItems.js
@@ -0,0 +1,105 @@
+export const topItem = {
+  {
+    key: 'item-0',
+    label: 'Top Item 0',
+    short: false,
+    icon: 'account-balance',
+    children: []
+}
+
+export const innerItem = {
+  key: 'item-0-0',
+  label: 'Item 0-0',
+  icon: 'query-builder'
+}
+
+export const items = {
+  top: [
+    {
+      key: 'item-0',
+      label: 'Top Item 0',
+      short: false,
+      icon: 'account-balance',
+      children: [
+        {
+          key: 'item-0-0',
+          label: 'Item 0-0',
+          icon: 'query-builder'
+        },
+        {
+          key: 'item-0-1',
+          label: 'Item 0-1',
+          icon: 'font-download'
+        },
+        {
+          key: 'item-0-2',
+          label: 'Item 0-2',
+          icon: 'gavel'
+        },
+        {
+          key: 'item-0-3',
+          label: 'Item 0-3',
+          icon: 'motorcycle'
+        }
+      ]
+    },
+    {
+      key: 'item-1',
+      label: 'Top Item 1',
+      short: true,
+      icon: 'account-circle'
+    },
+    {
+      key: 'item-2',
+      label: 'Top Item 2',
+      short: false,
+      icon: 'add-circle-outline'
+    },
+    {
+      key: 'item-3',
+      label: 'Top Item 3',
+      short: true,
+      icon: 'android',
+      children: [
+        {
+          key: 'item-3-0',
+          label: 'Item 3-0',
+          icon: 'euro-symbol'
+        }
+      ]
+    },
+    {
+      key: 'item-4',
+      label: 'Top Item 4',
+      short: false,
+      icon: 'bug-report'
+    }
+  ],
+  bottom: [
+    {
+      key: 'item-5',
+      label: 'Bottom Item 0',
+      short: true
+    },
+    {
+      key: 'item-6',
+      label: 'Bottom Item 1',
+      short: false
+    },
+    {
+      key: 'item-7',
+      label: 'Bottom Item 2',
+      short: true
+    },
+    {
+      key: 'item-8',
+      label: 'Bottom Item 3',
+      short: true
+    },
+    {
+      key: 'item-9',
+      label: 'Bottom Item 4',
+      short: false
+    }
+  ]
+}
