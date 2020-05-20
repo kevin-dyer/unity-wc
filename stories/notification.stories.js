5d13d184474c8ae3427a5868433ffe4fcf1bf812
Henry Raschke <hraschke@altair.com>
2020-05-20 13:12:22 -0700

    Added unity-notifications-handler to package.json; updated imports in notifications story

--- stories/notification.stories.js
+++ stories/notification.stories.js
@@ -1,6 +1,7 @@
 import '@bit/smartworks.unity.unity-core/unity-notification'
-import { addNotification } from '../src/components/unity-notification/unity-notifications-handler'
 import '@bit/smartworks.unity.unity-core/unity-button'
+import '@bit/smartworks.unity.unity-core/unity-notifications-handler'
+import { addNotification } from '@bit/smartworks.unity.unity-core/unity-notifications-handler'
 
 import { html } from 'lit-element';
 import { text, select, number, boolean, withKnobs } from "@storybook/addon-knobs";
