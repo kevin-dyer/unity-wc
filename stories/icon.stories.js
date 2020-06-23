b18a23235013bab5b0367e3f1769515ed8d566c6
Alberto <avicente@europe.altair.com>
2020-06-23 14:18:06 +0200

    unity-icon  | add src to allow custom images
    .

--- stories/icon.stories.js
+++ stories/icon.stories.js
@@ -7,13 +7,27 @@ export default {
   decorators: [withKnobs]
 }
 
-export const Icon = () => {
+export const UnityIcon = () => {
   const icon = text('Icon', 'unity:download')
   const color = text('Color', 'black')
+  const height = text('--unity-icon-height', '24px')
+  const width = text('--unity-icon-width', '24px')
   return html`
     <unity-icon
       icon=${icon}
-      style="color:${color};"
+      style="color:${color}; --unity-icon-height: ${height}; --unity-icon-width: ${width};"
+    >
+    </unity-icon>
+  `
+}
+
+export const ImageIcon = () => {
+  const height = text('--unity-icon-height', 'auto')
+  const width = text('--unity-icon-width', 'auto')
+  return html`
+    <unity-icon
+      src="https://www.altair.com/images/icons/Altair.svg"
+      style="--unity-icon-height: ${height}; --unity-icon-width: ${width};"
     >
     </unity-icon>
   `
