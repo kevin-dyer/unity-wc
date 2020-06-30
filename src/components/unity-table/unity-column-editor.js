0f98f4a645604f55bd714162184446b2fbebe44c
Kevin Dyer <kdyer@altair.com>
2020-06-29 19:57:43 -0700

    removed original buttonType prop from Column Editor example.

--- src/components/unity-table/unity-column-editor.js
+++ src/components/unity-table/unity-column-editor.js
@@ -14,7 +14,6 @@ import '@bit/smartworks.unity.unity-modal'
  * @param {[]} columns, array of objects - same as what is passed into unity-table
  * @param {[]} selectedColumns, array of column keys from columns that are visible
  * @param {func} onUpdate, callback that is sent an array of sorted visible columns
- * @param {''} buttonType, action button type for styling ('solid', 'gradient', 'outlined'), default ''
  * @returns {LitElement} returns a class extended from LitElement
  * @example
  *  <unity-column-editor
