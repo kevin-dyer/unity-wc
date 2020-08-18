f2ed21ae57776e2836961e0cbb58a8dcfc8f7f61
aheath <aheath@altair.com>
2020-08-18 12:50:02 -0700

    minor demo update for testing

--- src/components/my-text-input.js
+++ src/components/my-text-input.js
@@ -229,7 +229,7 @@ class MyTextInput extends PageViewElement {
           </div>
           <unity-search-bar
             search="search value"
-            .tags="${["tag 1", "tag 1.5", { label: "tag 2", value:"tag_2" }]}"
+            .tags="${["tag 1", "tag 1.5", { label: "tag_2", value:"tag_2" }]}"
             .tagSeed="${[{ label: "Tag 1", value: "tag 1" }, "Tag 2"]}"
             .onChange="${v => console.log('updating search to: ', v)}"
           ></unity-search-bar>
