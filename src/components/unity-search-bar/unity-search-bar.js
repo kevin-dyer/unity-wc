82bb4ac668f0a5dcec47cddfe03e82ca7317c33e
aheath <aheath@altair.com>
2020-08-26 17:22:03 -0700

    add missing import for typo for ellipsis

--- src/components/unity-search-bar/unity-search-bar.js
+++ src/components/unity-search-bar/unity-search-bar.js
@@ -5,6 +5,7 @@ import '@bit/smartworks.unity.unity-icon'
 import '@bit/smartworks.unity.unity-select-menu'
 import '@bit/smartworks.unity.unity-tag'
 import '@bit/smartworks.unity.unity-popover'
+import '@bit/smartworks.unity.unity-typography'
 import { findMatches } from '@bit/smartworks.unity.unity-utils'
 import { UnityDefaultThemeStyles } from '@bit/smartworks.unity.unity-default-theme-styles'
 
