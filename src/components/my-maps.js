c9b3a208995e6f378ed1c203651670403488e6f4
Denali Webber <dwebber@altair.com>
2020-09-22 11:51:19 -0700

    Testing moveable package, added dragging functionality for img

--- src/components/my-maps.js
+++ src/components/my-maps.js
@@ -1,47 +1,76 @@
 import { LitElement, html, css } from 'lit-element'
-import '@bit/smartworks.unity.unity-core/unity-icon'
-import '@openlayers-elements/maps/ol-map'
+import Moveable from 'moveable'
+import '@openlayers-elements/core/ol-map'
+// import '@openlayers-elements/maps/ol-map'
 import '@openlayers-elements/maps/ol-layer-openstreetmap'
 import '@openlayers-elements/core/ol-layer-vector'
 import '@openlayers-elements/maps/ol-marker-icon'
 import '@openlayers-elements/maps/ol-select'
-import '@deckdeckgo/drag-resize-rotate'
+import '@openlayers-elements/maps/ol-overlay'
 
 class MyMaps extends LitElement {
   firstUpdated() {
     this.zoomToFeature()
+    this.manipulateImage()
   }
 
   zoomToFeature() {
-    var map = this.shadowRoot.querySelector("#map");
+    var map = this.shadowRoot.querySelector("#map")
+    var floorplanOverlay = this.shadowRoot.querySelector("#floorplan")
 
     this.shadowRoot.querySelector('ol-select').addEventListener('feature-selected', (e) => {
-      console.log("POI was clicked!");
-      console.log(e.detail.feature.getGeometry())
+      console.log("POI was clicked!")
+      var coordinates = e.detail.feature.getGeometry().getFlatCoordinates()
       map.fit(e.detail.feature.getGeometry().getExtent(), {
-        duration:400,
+        // duration: 400,
         maxZoom: 18
       })
+      console.log(floorplanOverlay)
+      floorplanOverlay.setPosition(coordinates)
     })
+  }
+
+  manipulateImage() {
+    var floorplan = this.shadowRoot.querySelector('#madrid-floorplan')
+    const moveable = new Moveable(this.shadowRoot.querySelector('.map-page'), {
+      target: floorplan,
+      draggable:true,
+      // resizable: true,
+      // rotatable: true,
+    });
+
+    // draggable
+    moveable.on('drag', ({ target, transform, left, top}) => {
+      console.log(transform) // displays pixel translation
+      console.log("Left: ", left)
+      console.log("Top: ", top)
+      target.style.transform = transform;
+    });
+    // moveable.on('resize', ({ target, width, height }) => {
+    //   target.style.width = width + 'px';
+    //   target.style.height = height + 'px';
+    // });
+    // moveable.on('rotate', ({ target, transform }) => {
+    //   target.style.transform = transform;
+    // });
   }
 
   render() {
     return html`
     <div class="map-page">
       <h2>My Maps</h2>
-      <deckgo-drr
-        style="--width: 10%; --height:19%; --top:15%; --left:12.5%;">
-        <img src="https://www.roomsketcher.com/wp-content/uploads/2017/11/RoomSketcher-Order-Floor-Plans-2D-Floor-Plan.jpg">
-      </deckgo-drr>
-      <h2>End of drr test</h2>
+      <!-- <img id='madrid-floorplan' src='https://support.robinpowered.com/hc/article_attachments/360000862346/maps-before-plan.png' style='width:300px;height:200px'> -->
         <ol-map id="map" zoom="3" lat="16.231564" lon="-28.992471">
         <ol-layer-openstreetmap></ol-layer-openstreetmap>
-          <ol-layer-vector z-index="1">
+          <ol-layer-vector id="markers" z-index="1">
             <ol-marker-icon id="Madrid Office" src="https://github.com/zazuko/openlayers-elements/blob/master/demos/assets/icon.png?raw=true" lon="-3.632980" lat="40.431490" scale="0.5"></ol-marker-icon>
             <ol-marker-icon id="Berkeley Office" src="https://github.com/zazuko/openlayers-elements/blob/master/demos/assets/icon.png?raw=true" lon="-122.268340" lat="37.871170" scale="0.5"></ol-marker-icon>
             <ol-marker-icon id="Troy Office" src="https://github.com/zazuko/openlayers-elements/blob/master/demos/assets/icon.png?raw=true" lon="-83.111720" lat="42.562050" scale="0.5"></ol-marker-icon>
           </ol-layer-vector>
           <ol-select></ol-select>
+          <ol-overlay id="floorplan">
+            <img id="madrid-floorplan" src="https://support.robinpowered.com/hc/article_attachments/360000862346/maps-before-plan.png" style="width:200px;height:100px">
+          </ol-overlay>
         </ol-map>
     </div>`
   }
