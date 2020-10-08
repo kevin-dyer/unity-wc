4ffc2dd57c8df825671c549aa294b5ba0d5da589
aheath <aheath@altair.com>
2020-10-07 19:31:52 -0700

    add rest of styles, implement all vars, change indeterminate to be separate from checked

--- src/components/unity-checkbox/unity-checkbox.js
+++ src/components/unity-checkbox/unity-checkbox.js
@@ -2,13 +2,14 @@ import { LitElement, html, css } from 'lit-element'
 import '@spectrum-web-components/checkbox/sp-checkbox.js'
 
 import { UnityDefaultThemeStyles } from '@bit/smartworks.unity.unity-default-theme-styles'
+import '@bit/smartworks.unity.unity-typography'
 
 /**
  * Displays three state checkmark
  * @name UnityCheckbox
  * @param {string} label, label to render next to the checkbox
  * @param {boolean} checked, whether the checkbox is checked
- * @param {boolean} indeterminate, whether the checkbox is in the indeterminate state, must also be checked
+ * @param {boolean} indeterminate, whether the checkbox is in the indeterminate state, overrides checked
  * @param {boolean} disabled, whether the checkbox is disabled
  * @return {LitElement} return s aclass extended from LitElement
  * @example
@@ -41,8 +42,19 @@ class UnityCheckbox extends LitElement {
   }
 
   render() {
+    const {
+      label,
+      checked,
+      indeterminate,
+      disabled
+    } = this
     return html`
-      <sp-checkbox></sp-checkbox>
+      <sp-checkbox
+        class="checkbox${disabled ? " disabled" : ""}"
+        ?disabled="${disabled ? true : null}"
+        ?checked="${checked ? true : null}"
+        ?indeterminate="${indeterminate ? true: null}"
+      ><unity-typography>${label}</unity-typography></sp-checkbox>
     `
   }
 
@@ -52,19 +64,38 @@ class UnityCheckbox extends LitElement {
       UnityDefaultThemeStyles,
       css`
         :host {
-          --spectrum-checkbox-height: 16px;
           --spectrum-checkbox-box-size: 16px;
           --spectrum-checkbox-box-border-radius: 1px;
           --spectrum-checkbox-box-border-size: 1px;
-          --spectrum-icon-checkmark-small-height: 11px;
+          --spectrum-icon-checkmark-small-height: 14px;
           --spectrum-icon-checkmark-small-width: 14px;
+          --spectrum-icon-dash-small-height: 14px;
+          --spectrum-icon-dash-small-width: 14px;
+          --spectrum-checkbox-text-gap: 4px;
 
+          /* Unselected */
           --spectrum-checkbox-emphasized-box-background-color: var(--white-color, var(--default-white-color));
-
           --spectrum-checkbox-emphasized-box-border-color: var(--gray-color, var(--default-gray-color));
+          --spectrum-checkbox-emphasized-box-border-color-hover: var(--spectrum-checkbox-emphasized-box-border-color);
+          --spectrum-checkbox-emphasized-box-border-color-down: var(--spectrum-checkbox-emphasized-box-border-color);
 
+          /* Selected */
           --spectrum-checkbox-checkmark-color: var(--spectrum-checkbox-emphasized-box-background-color);
           --spectrum-checkbox-emphasized-box-border-color-selected: var(--primary-color, var(--default-primary-color));
+          --spectrum-checkbox-emphasized-box-border-color-selected-hover: var(--primary-shade-color, var(--default-primary-shade-color));
+          --spectrum-checkbox-emphasized-box-border-color-selected-down: var(--primary-shade-color, var(--default-primary-shade-color));
+
+          /* Disabled */
+          --spectrum-checkbox-emphasized-box-background-color-disabled: var(--light-gray-2-color, var(--default-light-gray-2-color));
+          --spectrum-checkbox-emphasized-box-border-color-disabled: var(--spectrum-checkbox-emphasized-box-border-color);
+        }
+
+        sp-checkbox.checkbox:hover {
+          --spectrum-checkbox-emphasized-box-background-color: var(--primary-tint-1-color, var(--default-primary-tint-1-color));
+        }
+
+        sp-checkbox..checkbox.disabled {
+          --spectrum-checkbox-checkmark-color: var(--gray-color, var(--default-gray-color));
         }
       `
     ]
