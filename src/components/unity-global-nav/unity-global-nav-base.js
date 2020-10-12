05ea5f1a2ef38933bbb41abe987088f4d1b0c7a4
pirouzm <pmehmandoost@altair.com>
2020-10-12 09:21:55 -0700

    Change conditional to place logo, to only have a wrapper div for logo if logo prop is passed.

--- src/components/unity-global-nav/unity-global-nav-base.js
+++ src/components/unity-global-nav/unity-global-nav-base.js
@@ -119,14 +119,16 @@ class UnityGlobalNavBase extends LitElement {
     return html`
         <div class="menu text${collapsed?' collapsed':''}${gutter?' gutter':''}${_showGrid? ' shadowless': ''}">
           <div class="header-container">
-            <slot name="customHeader"></slot>
+            <slot name="customHeader">
+          
+            </slot>
+            <slot>
             ${logo?
               html`<div class="logo-container flex-center ${grid? 'clickable': ''}" @click=${grid? () => this._toggleGrid() : null}>
                 <div class="logo">
                   <img src=${logo}>
                 </div>
-              </div>`
-              : ''}
+              </div>`: ''}
               ${!collapsed?
                   headerImg?
                     html`<img style="padding: 0 var(--global-nav-padding-size-sm, var(--default-global-nav-padding-size-sm));" src=${headerImg}>` :
