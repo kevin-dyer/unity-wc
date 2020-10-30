f0b5ac499b5fced3fcc509ce3a9e7c3c9dff709d
aheath <aheath@altair.com>
2020-10-30 15:48:35 -0700

    add new dark gray, brighter than charcoal

--- src/components/unity-theme-styles.js
+++ src/components/unity-theme-styles.js
@@ -10,6 +10,7 @@ export const UnityThemeStyles = css`
     --black-rgb: 0, 0, 0;                   /* #000000 */
     --white-rgb: 255, 255, 255;             /* #FFFFFF */
     --charcoal-rgb: 77, 77, 77;             /* #4D4D4D */
+    --dark-gray-rgb: 151, 151, 151;         /* #979797 */
     --gray-rgb: 162, 170, 173;              /* #A2AAAD */
     --light-gray-1-rgb: 235, 235, 235;      /* #EBEBEB */
     --light-gray-2-rgb: 244, 244, 244;      /* #F4F4F4 */
@@ -36,6 +37,9 @@ export const UnityThemeStyles = css`
 
     /* dark grey, text default */
     --dark-gray-color: rgb(var(--charcoal-rgb));
+
+    /* dark grey 2 */
+    --dark-gray-2-color: rgb(var(--default-dark-gray-rgb));
 
     /* normal gray, border default, disabled text */
     --gray-color: rgb(var(--gray-rgb));
