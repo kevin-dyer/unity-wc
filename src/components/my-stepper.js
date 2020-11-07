88c8afb2321a1653bef5670087ff65aa6ed7bcea
aheath <aheath@altair.com>
2020-11-06 18:13:12 -0800

    add totalSteps tracker

--- src/components/my-stepper.js
+++ src/components/my-stepper.js
@@ -77,6 +77,7 @@ class MyStepper extends PageViewElement {
       <div class="example-container">
         <unity-stepper
           .steps="${steps}"
+          totalSteps="5"
           .onChangeStep="${step => console.log('step', step)}"
           backtrack
           valid
