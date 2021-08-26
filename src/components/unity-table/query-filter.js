fddc96e6ab97dd5b814e3fc6b0fda31841c842ff
Kevin Dyer <kdyer@altair.com>
2021-08-26 13:23:26 -0700

    change operation to operator in query-filter. user operators prop for dropdown options.

--- src/components/unity-table/query-filter.js
+++ src/components/unity-table/query-filter.js
@@ -4,37 +4,12 @@ import '@bit/smartworks.unity.unity-text-input'
 import '@bit/smartworks.unity.unity-dropdown'
 import { UnityDefaultThemeStyles } from '@bit/smartworks.unity.unity-default-theme-styles'
 
-const operationOptions = [
-  {
-    "label": "==",
-    "id": "eq"
-  },
-  {
-    "label": "!=",
-    "id": "neq"
-  },
-  {
-    "label": ">",
-    "id": "gt",
-  },
-  {
-    "label": ">=",
-    "id": "gte",
-  },
-  {
-    "label": "<",
-    "id": "lt",
-  },
-  {
-    "label": "<=",
-    "id": "lte",
-  }
-]
 
 class QueryFilter extends LitElement {
   constructor() {
     super()
-    this.operation = ""
+    this.operator = ""
+    this.operators = []
     this.value = ""
     this.onValueChange = () => {};
     this.onDelete = () => {};
@@ -44,17 +19,17 @@ class QueryFilter extends LitElement {
 
   static get properties() {
     return {
-      options: { type: Array },
-      operation: { type: String },
+      operator: { type: String },
       value: {type: String},
+      operators: { type: Array },
       _valueError: {type: String},
       onValueChange: { type: Function },
       onDelete: { type: Function },
     }
   }
 
-  onOperationSelect(operation) {
-    this.operation = operation
+  onOperationSelect(operator) {
+    this.operator = operator
     this.handleFilterChange()
   }
 
@@ -70,7 +45,7 @@ class QueryFilter extends LitElement {
   }
 
   validate() {
-    if (/gt|gte|lt|lte/.test(this.operation)) {
+    if (/gt|gte|lt|lte/.test(this.operator)) {
 
       if (!this.value || isNaN(this.value)) {
         this._valueError = 'Number needed'
@@ -85,7 +60,7 @@ class QueryFilter extends LitElement {
   handleFilterChange() {
     const isValid = this.validate()
     this.onValueChange({
-      operation: this.operation,
+      operator: this.operator,
       value: this.value
     })
   }
@@ -99,8 +74,8 @@ class QueryFilter extends LitElement {
           label=""
           inputType="single-select"
           placeholder="oper"
-          .options=${operationOptions}
-          .selected="${[this.operation]}"
+          .options="${this.operators}"
+          .selected="${[this.operator]}"
           .onValueChange=${this.onOperationSelect.bind(this)}
         >
         </unity-dropdown>
